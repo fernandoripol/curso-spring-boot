40a73c1af318091539e022c33bfdb2aaa388a691
Fernando Ripol <fernandorodriguesripol@gmail.com>
2026-09-19 19:18:59 -0300

    feat: implementa entidade User e configura H2 database

--- course/src/main/java/com/educandoweb/course/entities/User.java
+++ course/src/main/java/com/educandoweb/course/entities/User.java
@@ -1,19 +1,32 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public class User implements  Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-    private  Long id;
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public User(){        
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
+        super();
         this.id = id;
         this.name = name;
         this.email = email;
@@ -63,10 +76,7 @@ public class User implements  Serializable{
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+        return Objects.hash(id);
     }
 
     @Override
@@ -78,17 +88,6 @@ public class User implements  Serializable{
         if (getClass() != obj.getClass())
             return false;
         User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+        return Objects.equals(id, other.id);
     }
-
-    
-
-
-
-
 }
